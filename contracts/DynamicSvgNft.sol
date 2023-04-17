6b722d8350d8eaef60731ab39397c97cacc8317a
Psevdon1m <Valentine.oleynik@gmail.com>
2023-04-17 22:46:04 +0300

    add base erc 721 w/ svg uri, add svg images

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -3,7 +3,20 @@ pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
+import "@openzeppelin/contracts/tokens/ERC721/ERC721.sol";
 
-contract DynamicSvgNft {
+contract DynamicSvgNft is ERC721 {
+    uint public tokenCounter;
+    string private i_lowImageUri;
+    string private i_highImageUri;
 
+    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG", "DSVG") {
+        i_lowImageUri = lowSvg;
+        i_highImageUri = highSvg;
+    }
+
+    function mint() public {
+        _safeMint(msg.sender, tokenCounter);
+        tokenCounter++;
+    }
 }
